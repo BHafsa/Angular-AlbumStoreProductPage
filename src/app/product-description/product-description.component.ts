d0c87b69c4f2e69b498f8ad2067c473154e64d90
BHafsa <eh_bousbiat@esi.dz>
2019-10-25 11:35:44 +0200

    part3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,6 @@
 
 
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ProductService} from '../product.service'
 @Component({
     selector :'app-product-description',
@@ -8,7 +8,7 @@ import {ProductService} from '../product.service'
     styleUrls: ['./product-description.component.css']
 })
 
-export class ProductDescriptionComponent{
+export class ProductDescriptionComponent implements OnInit{
     albumInfo ;
 
 
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -4,7 +4,7 @@ import {Http, Response } from '@angular/http'
 import 'rxjs/add/operator/map' ;
 
 @Injectable({
-  providedIn: 'root',
+  providedIn : 'root'
   
 })
 

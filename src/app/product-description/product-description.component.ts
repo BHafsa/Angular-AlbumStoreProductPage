89efbd91e42ba2138ccf516473b0cfa50902f373
BHafsa <eh_bousbiat@esi.dz>
2019-10-25 11:15:06 +0200

    part3

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,7 +1,7 @@
 
 
 import {Component} from '@angular/core';
-
+import {ProductService} from '../product.service'
 @Component({
     selector :'app-product-description',
     templateUrl :'./product-description.component.html',

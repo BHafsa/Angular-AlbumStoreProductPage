e7c2fb082604dc64bab09858dc64a237212791dd
BHafsa <eh_bousbiat@esi.dz>
2019-10-25 13:05:34 +0200

    part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { ProductTracklistingComponent } from './product-tracklisting/product-tra
 import { RouterModule, Routes} from '@angular/router'
 import { ProductListComponent } from './product-list/product-list.component';
 
-const appRoutes : Routes =[
+const appRoutes: Routes =[
   {
     path:'products',
     component:ProductListComponent,
